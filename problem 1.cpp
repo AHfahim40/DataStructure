6aaa2cdfae46d689d91d6f54d01d1d0d5b5386b2
AH Fahim <36567376+AHfahim40@users.noreply.github.com>
2018-05-24 14:37:43 +0600

    Add files via upload

---
+++ problem 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+    int a[100];
+    int i,n;
+    cout<<"Enter the number of inputs"<<endl;
+    cin>>n;
+
+    for(i=0;i<n;i++)
+        cin>>a[i];
+
+        for(i = 0 ; i < n ; i ++){
+            cout<<a[i];
+        }
+
+
+    }
